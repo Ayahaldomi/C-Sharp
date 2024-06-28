44166bb11b07b5b9e7462453f19ebf53ff4ca435
Ayahaldomi <ayahaldomi@gmail.com>
2024-06-28 15:41:26 +0300

    task

---
+++ 27-06/27-06/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace _27_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1 - Correct the syntax error:
+
+            //•	int[] ARR = {1, 7, 9,  45};
+            //•	string[] arr2 = {"Str", "alex","moh"};
+            //•	string[] arr3= {"the","fox", "over", "lazy", "dog"};
+
+
+
+            //2 - What the index of "Banana","Tomato" ?
+
+            String[] fruits = { "Tomato", "Banana", "Watermelon" };
+            Console.WriteLine(Array.IndexOf(fruits, "Banana"));
+            Console.WriteLine(Array.IndexOf(fruits, "Tomato"));
+
+
+
+            //3 - Create an multiple arrays that represents your:
+            //•	Favorite Food(5 item)
+            //•	Favorite Sport(3 item)
+            //•	Favorite Movie(4 item)
+            //Then, print each array using foreach, and Loop Through an Array
+
+
+            Console.WriteLine("MY FAVORITE !!");
+            string[] favFood = { "apple", "mango", "banana", "water", "coco" };
+            foreach (var food in favFood) { Console.WriteLine(food); }
+            string[] favSport = { "soccer", "basketball", "football" };
+            foreach (var sport in favSport) { Console.WriteLine(sport); };
+            string[] favMovies = { "The First Omen", "Unlocked", "Trigger Warning", "The Hunger Games" };
+            foreach (var movies in favMovies) { Console.WriteLine(movies); };
+
+
+            //4 - Write a program in C# to calculate the sum of three numbers with getting input in one line separated by a comma
+            //•	Expected Output:
+            //•	Input three numbers separated by comma: 5,10,15
+            //•	The sum of three numbers: 30.
+
+            Console.Write("Input three numbers separated by comma: ");
+            string nums = Console.ReadLine();
+            string[] num = nums.Split(',');
+
+            int sum = 0;
+            foreach (var i in num)
+            {
+                int number = int.Parse(i);
+                sum += number;
+            }
+            Console.WriteLine("The sum of three numbers: " + sum);
+
+
+            //5 - Write a program in C# to display the n terms of odd number and their sum from [1- 100 ]. 
+            //•	Test Data
+            //•	The odd numbers are1 3 5 7 9 11 13 15 17 19……
+            //•	The Sum of odd Numbers is: …...
+
+
+            Console.Write("The odd numbers are1 ");
+            for (int i = 1; i <= 100; i += 2)
+            {
+                if (i == 99)
+                {
+                    Console.WriteLine(" " + i);
+                    continue;
+                }
+                Console.Write(" " + i);
+            }
+            int sum1 = 0;
+            for (int i = 1; i <= 100; i += 2)
+            {
+                sum1 += i;
+            }
+            Console.Write("The Sum of odd Numbers is:");
+            Console.WriteLine(sum1);
+
+
+            //6 - Write a program in C## to display the pattern like right angle triangle using an asterisk. Go to the editor
+            //The pattern like:
+            //   *
+            //  **
+            // ***
+            //****
+
+            int k = 1;
+            while (k <= 5)
+            {
+                int j = 1;
+                while (j < k)
+                {
+
+                    Console.Write("*");
+                    j++;
+                }
+                k++;
+                Console.WriteLine();
+            }
+            //7 - Write a program in C# to make such a pattern like right angle triangle with number increased by 1. Go to the editor
+            //The pattern like :
+
+            //     1
+            //   2 3
+            //  4 5 6
+            //7 8 9 10
+
+            int counter = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(counter++);
+                }
+
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
--- task26-06/task26-06/Program.cs
+++ task26-06/task26-06/Program.cs
@@ -13,9 +13,11 @@ namespace task26_06
         {
             //1.Write a C# program that accept two integers and display the smaller.
             //
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            //int num2 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("the smaller number is : "); Console.WriteLine(Math.Min(num1, num2));
+
+            Console.WriteLine("task 1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("the smaller number is : "); Console.WriteLine(Math.Min(num1, num2));
 
 
 
@@ -24,6 +26,7 @@ namespace task26_06
             //•	Sample numbers: -7
             //•	Output: The sign is -
 
+            Console.WriteLine("task 2");
             int number = Convert.ToInt32(Console.ReadLine());
             string sign = number.ToString("+#;-#;0");
 
@@ -36,10 +39,11 @@ namespace task26_06
             //•	Sample numbers: 0, -1, 4
             //•	Output: 4, 0, -1
 
-            //int[] ints = { 0, -1, 4 };
-            //Array.Sort(ints);
-            //Array.Reverse(ints);
-            //Console.Write(ints[0]); Console.Write(" "); Console.Write(ints[1]); Console.Write(" "); Console.WriteLine(ints[2]);
+            Console.WriteLine("task 3");
+            int[] ints = { 0, -1, 4 };
+            Array.Sort(ints);
+            Array.Reverse(ints);
+            Console.Write(ints[0]); Console.Write(" "); Console.Write(ints[1]); Console.Write(" "); Console.WriteLine(ints[2]);
 
 
 
@@ -48,10 +52,11 @@ namespace task26_06
             //•	Sample numbers: -5, -2, -6, 0, -1
             //•	Output: 0
 
-            //int[] ints = { -5, -2, -6, 0, -1 };
-            //Array.Sort(ints);
-            //Array.Reverse(ints);
-            //Console.WriteLine(ints[0]);
+            Console.WriteLine("task 4");
+            int[] ints1 = { -5, -2, -6, 0, -1 };
+            Array.Sort(ints1);
+            Array.Reverse(ints1);
+            Console.WriteLine(ints1[0]);
 
 
 
@@ -59,9 +64,9 @@ namespace task26_06
             //Expected Output:
             //•	Input kilometers per hour: 15.
             //•	9.320568 miles per hour
-
-            //Console.Write("Input kilometers per hour: "); int kilo = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(kilo * 0.621371 + " miles per hour");
+            Console.WriteLine("task 5");
+            Console.Write("Input kilometers per hour: "); int kilo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(kilo * 0.621371 + " miles per hour");
 
 
 
@@ -71,31 +76,38 @@ namespace task26_06
             //•	Input minutes: 37
             //•	Total: 337 minutes.
 
-            //Console.Write("Input hours: ");int hours = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Input minutes: "); int minutes = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"Total: {hours * 60 + minutes}");
+            Console.WriteLine("task 6");
+            Console.Write("Input hours: "); int hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input minutes: "); int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Total: {hours * 60 + minutes}");
 
 
 
             //7.Write a program in C# that takes minutes as input and display the total number of hours and minutes. 
             //•	Input minutes: 546
             //•	9 Hours, 6 Minutes
-
-            //Console.Write("Input minutes: ");int minutes = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"{minutes / 60} Hours, {minutes % 60} Minutes");
+            Console.WriteLine("task 7");
+            Console.Write("Input minutes: "); int minutes1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{minutes1 / 60} Hours, {minutes1 % 60} Minutes");
 
 
 
 
             //8.Create string of array that contains 5 element of different sentence with different length.
             //Print all elements with fixed length for each sentence.
+            Console.WriteLine("task 8");
+            string[] strings = { "my name is ayah", "orange coding ", "irbid is the best", "i love mansef", "i need to sleep" };
+            Console.WriteLine(strings[0] + $" >>>The length of this sentence is : {strings[0].Length}");
+            Console.WriteLine(strings[1] + $" >>>The length of this sentence is : {strings[1].Length}");
+            Console.WriteLine(strings[2] + $" >>>The length of this sentence is : {strings[2].Length}");
+            Console.WriteLine(strings[3] + $" >>>The length of this sentence is : {strings[3].Length}");
+            Console.WriteLine(strings[4] + $" >>>The length of this sentence is : {strings[4].Length}");
 
-            //string[] strings = { "my name is ayah", "orange coding ", "irbid is the best", "i love mansef", "i need to sleep" };
-            //Console.WriteLine(strings[0] + $" >>>The length of this sentence is : {strings[0].Length}");
-            //Console.WriteLine(strings[1] + $" >>>The length of this sentence is : {strings[1].Length}");
-            //Console.WriteLine(strings[2] + $" >>>The length of this sentence is : {strings[2].Length}");
-            //Console.WriteLine(strings[3] + $" >>>The length of this sentence is : {strings[3].Length}");
-            //Console.WriteLine(strings[4] + $" >>>The length of this sentence is : {strings[4].Length}");
+            Console.WriteLine(strings[0].Substring(0, 7));
+            Console.WriteLine(strings[1].Substring(0, 7));
+            Console.WriteLine(strings[2].Substring(0, 7));
+            Console.WriteLine(strings[3].Substring(0, 7));
+            Console.WriteLine(strings[4].Substring(0, 7));
 
 
 
